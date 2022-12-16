ff915aa806d4ad7071a9fbfa9cd6aa90646c4893
Tareq Assiri <githubtareq@gmail.com>
2022-12-16 19:55:21 +0300

    fixed diagnostics config

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,5 +1,24 @@
 vim.opt.signcolumn = 'yes' -- Reserve space for diagnostic icons
 local lsp = require('lsp-zero')
-
 lsp.preset('recommended')
+lsp.nvim_workspace()
 lsp.setup()
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+local signs = {
+  Error = '✘',
+  Warn = '▲',
+  Hint = '⚑',
+  Info = ''
+}
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
