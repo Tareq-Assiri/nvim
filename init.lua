9fd52b6fe4cb6a045c6af6476bb0168b87f68167
Tareq Assiri <githubtareq@gmail.com>
2022-12-16 16:13:33 +0300

    added lsp and snippets

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("core.keymaps")
 require("core.plugin_config")
 require("core.plugins")
+
--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -13,7 +13,8 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
-
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("lspconfig").sumneko_lua.setup {
-  on_attach = on_attach
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
